63f29876258dc462dac1c3b46295b93eca267f5c
Varun Santhanam <varun.santhanam@sherbit.io>
2018-02-02 14:38:38 -0800

    more manometer basics

---
+++ Release/StatioKit.framework/Versions/A/Headers/SKMagnometerManager.h
@@ -0,0 +1,43 @@
+//
+//  SKMagnometerManager.h
+//  StatioKit
+//
+//  Created by Varun Santhanam on 2/2/18.
+//  Copyright © 2018 Varun Santhanam. All rights reserved.
+//
+
+@import Foundation;
+
+typedef struct {
+    
+    NSTimeInterval timestamp;
+    double x;
+    double y;
+    double z;
+    
+} SKMagneticFieldSample;
+
+@class SKMagnometerManager;
+
+@protocol SKMagnometerManagerDelegate<NSObject>
+
+- (void)magnometerManager:(nonnull SKMagnometerManager *)manager didRecieveMagneticFieldSample:(SKMagneticFieldSample)magneticFieldSample;
+- (void)magnometerManager:(nonnull SKMagnometerManager *)manager didFailToGetMagnometerDataWithError:(nonnull NSError *)error;
+
+@end
+
+@interface SKMagnometerManager : NSObject
+
+@property (weak, nullable) id<SKMagnometerManagerDelegate> delegate;
+@property (NS_NONATOMIC_IOSONLY, readonly, getter=isTracking) BOOL tracking;
+@property (NS_NONATOMIC_IOSONLY, assign, readonly) SKMagneticFieldSample magneticFieldSample;
+
+- (nullable instancetype)initWithDelegate:(nullable id<SKMagnometerManagerDelegate>)delegate;
+
+- (BOOL)startTracking;
+- (BOOL)startTrackingWithUpdateFrequency:(double)frequency;
+- (BOOL)stopTracking;
+
+NSString * _Nonnull NSStringFromSKMagneticFieldSample(SKMagneticFieldSample magneticFieldSample);
+
+@end
