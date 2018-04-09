380f089e4ca88feb3a4814c068b1573059c2d483
Varun Santhanam <talkto@vsanthanam.com>
2018-04-08 20:31:37 -0700

    test

---
+++ StatioKit/SKLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  SKLocationManager.h
+//  StatioKit
+//
+//  Created by Varun Santhanam on 4/8/18.
+//  Copyright © 2018 Varun Santhanam. All rights reserved.
+//
+
+@import Foundation;
+
+@interface SKLocationManager : NSObject
+
+@end
---
+++ StatioKit/SKLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  SKLocationManager.m
+//  StatioKit
+//
+//  Created by Varun Santhanam on 4/8/18.
+//  Copyright © 2018 Varun Santhanam. All rights reserved.
+//
+
+#import "SKLocationManager.h"
+
+@implementation SKLocationManager
+
+@end
